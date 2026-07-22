2c74d8b4e7a03590563e3db4c91e50ea03371834
yami <1033998700@qq.com>
2026-07-22 23:29:22 +0800

    feat: preload script with contextBridge IPC

---
+++ electron/preload.js
@@ -0,0 +1,14 @@
+// electron/preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  readFile: (fp) => ipcRenderer.invoke('file:read', fp),
+  writeFile: (fp, content) => ipcRenderer.invoke('file:write', fp, content),
+  openFileDialog: () => ipcRenderer.invoke('file:open-dialog'),
+  listDir: (dirPath) => ipcRenderer.invoke('dir:list', dirPath),
+  exportPDF: () => ipcRenderer.invoke('export:pdf'),
+  getSystemTheme: () => ipcRenderer.invoke('theme:get-system'),
+  onMenuAction: (cb) => { ipcRenderer.on('menu:action', (_e, action) => cb(action)); },
+  onFileOpened: (cb) => { ipcRenderer.on('file:opened', (_e, data) => cb(data)); },
+  showError: (title, msg) => ipcRenderer.invoke('dialog:show-error', title, msg),
+});
